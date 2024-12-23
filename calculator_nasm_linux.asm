c208b8452481c4b49e303b557655a6b2fdf03c49
Kristina <31845754+HopelessCompass@users.noreply.github.com>
2024-12-23 13:05:01 +0300

    Вызов ядра из функции

--- calculator_nasm_linux.asm
+++ calculator_nasm_linux.asm
@@ -28,7 +28,7 @@ mov eax, 4 ; Номер системного вызова: sys_write
 mov ebx, 1 ; Дескриптор файла: 1 (stdout)
 mov ecx, hello ; Сохраняем в регистре сообщение для вывода в терминал
 mov edx, hello_len ; сохраняем в регистре ДЛИНУ сообщения для вывода в терминал
-int 0x80 ; вызов ядра
+call_kernel ; вызов ядра
 
 ; Запрос двух чисел
 call get_first_number
@@ -46,23 +46,22 @@ call get_second_number
 ; Завершаем программу
 mov eax, 1          ; Номер системного вызова: sys_exit
 xor ebx, ebx        ; Код возврата: 0
-int 0x80            ; Вызов ядра
+call_kernel            ; Вызов ядра
 
 greetings:
     ; Пишем строку в stdout
     ; call output
     mov ecx, hello      ; Адрес строки
     mov edx, hello_len  ; Длина строки
-    int 0x80            ; Вызов ядра
+    call_kernel            ; Вызов ядра
     ret
 
 get_first_number:
     ; выводим сообщение с просьбой ввода первого числа из переменной
-    mov eax, 4 ; Номер системного вызова: sys_write
-    mov ebx, 1 ; Дескриптор файла: 1 (stdout)
+    call stdout_prep
     mov ecx, prompt1 ; сохраняем в регистре сообщение для вывода в терминал
     mov edx, prompt1_len ; сохраняем в регистре ДЛИНУ сообщения для вывода в терминал
-    int 0x80 ; вызов ядра
+    call_kernel ; вызов ядра
 
     mov input, first ; назначаем временной переменной input значение поля ввода для функции чтения из stdin
 
@@ -72,11 +71,10 @@ get_first_number:
 
 get_second_number:
     ; выводим сообщение с просьбой ввода первого числа из переменной
-    mov eax, 4 ; Номер системного вызова: sys_write
-    mov ebx, 1 ; Дескриптор файла: 1 (stdout)
+    call stdout_prep
     mov ecx, prompt2 ; сохраняем в регистре сообщение для вывода в терминал
     mov edx, prompt2_len ; сохраняем в регистре ДЛИНУ сообщения для вывода в терминал
-    int 0x80 ; вызов ядра
+    call_kernel ; вызов ядра
 
     mov input, second ; назначаем временной переменной input значение поля ввода для функции чтения из stdin
 
@@ -84,13 +82,20 @@ get_second_number:
     call second_ascii_to_number
     ret
 
+stdout_prep:
+    mov eax, 4 ; sys_write
+    mov ebx, 1 ; stdout
+
+call_kernel:
+    call_kernel ; вызов ядра
+
 read_from_stdin:
     ; считать строку из stdin
     mov eax, 3 ; запрос sys_read
     mov ebx, 0 ; stdin
     mov ecx, input ; буфер, назначается из функций get... для чисел
     mov edx, 7 ; максимальная длина
-    int 0x80 ; вызов ядра
+    call_kernel ; вызов ядра
     mov [len], eax ; выгрузить из регистра длину ввода
     ret
 
